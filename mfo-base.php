03df8aa13b52f28fe14056f4dd56e7d4423d0600
Ian Cole <ian.cole@gmail.com>
2016-09-03 08:34:37 -0700

    Educator code email updates

--- mfo-base.php
+++ mfo-base.php
@@ -4,7 +4,7 @@
 Plugin Name: Maker Faire Online - CFM & More
 Plugin URI: http://www.makerfaireorlando.com
 Description: Helper plugin for the Maker Faire Online system based using the Toolset plugins & more
-Version: 3.19.1
+Version: 3.19.2
 Author: Ian Cole (Maker Faire Orlando)
 Author URI: http://www.themakereffect.org/about/
 GitHub Plugin URI: digitalman2112/mfo-wordpress-plugin
@@ -73,6 +73,7 @@ Changelog:
 08-08-2016: 3.18.3: Added cleanup function to reset current year exhibits to new helper min quantity
 09-03-2016: 3.19.0: Added eventbrite code creation and email for educators
 09-03-2016: 3.19.1: Minor updates to eventbrite educator code emails
+09-03-2016: 3.19.2: Minor updates to eventbrite educator code emails
 */
 
 
