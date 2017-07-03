12a759fd1f184322ad69d6b588189d522b469194
Ian Cole <ian.cole@gmail.com>
2017-07-03 02:12:03 +0000

    Fixed email link; added check for unapproved exhibits

--- templates/single-exhibit.php
+++ templates/single-exhibit.php
@@ -2,7 +2,10 @@
 /**
  * single-exhibit.php
  * Page Template for exhibit custom post type
- * todo: add back in the not-approved message
+ * todo: add check for approval status &  message
+ * todo: add categories
+ * todo: add additional photos / videos
+ * todo: exhibit location (with check for setting)
  */
 
 get_header(); ?>
@@ -13,12 +16,13 @@ get_header(); ?>
 
 		<?php
 		  $exhibit_id = get_the_id();
+		  $approval_status = get_post_meta($exhibit_id, 'wpcf-approval-status', true);
 		  $maker_id = wpcf_pr_post_get_belongs($exhibit_id, 'maker');
 		  $maker = get_post($maker_id);
 		?>
-                <div id="post-<?php the_ID(); ?>" <?php post_class('mfo-single-exhibit'); ?>>
-			 <?php if ( (get_the_author_meta('ID') == get_current_user_id() )
-                                         || current_user_can("edit_posts", get_the_ID() )):?>
+                <div id="post-<?php the_ID(); ?>" <?php post_class('mfo-single-exhibit approval-status-'.$approval_status); ?>>
+			 <?php if ( ($approval_status ==1 ) || (get_the_author_meta('ID') == get_current_user_id() )
+                                           || current_user_can("edit_posts", get_the_ID() )):?>
                         <div class="container">
                         <div class="row">
 			    <div class="content entry-page col-xs-12">
@@ -49,7 +53,7 @@ get_header(); ?>
 				<?php if ($website) echo '<div class="col-md-2 pull-left"><a href="'. get_post_meta($exhibit_id, 'wpcf-website', true) . '" class="btn btn-info" target="_blank">Project Website</a></div>'; ?>
 				<div class="col-md-5">
 				<ul class="list-inline">
-				<?php if ($email) 	echo '<li><a href="' . $email	   . '" target="_blank"<i class="fa fa-2x fa-envelope" 	aria-hidden="true"></i></a></li>'; ?>
+				<?php if ($email) 	echo '<li><a href="mailto:' . $email	   . '" target="_blank"<i class="fa fa-2x fa-envelope" 	aria-hidden="true"></i></a></li>'; ?>
 				<?php if ($twitter) 	echo '<li><a href="' . $twitter    . '" target="_blank"<i class="fa fa-2x fa-twitter" 	aria-hidden="true"></i></a></li>'; ?>
 				<?php if ($instagram) 	echo '<li><a href="' . $instagram  . '" target="_blank"<i class="fa fa-2x fa-instagram" 	aria-hidden="true"></i></a></li>'; ?>
 				<?php if ($facebok) 	echo '<li><a href="' . $facebook   . '" target="_blank"<i class="fa fa-2x fa-facebook" 	aria-hidden="true"></i></a></li>'; ?>
@@ -92,9 +96,11 @@ get_header(); ?>
 
                         </div><!-- .entry-content -->
                         <?php else: ?>
-                        <div style="height: 200px; margin-top: 40px;"><strong>This Exhibit is not yet approved. If this is your Exhibit and are trying to view it, please 
-                        <a href="/wp-login/">login</a>.</strong></div>
-                        <?php endif ?>
+			<div style="height:200px">
+			<div class="container"><h2>This exhibit has not been approved. If you are receiving this message in error, please email us - <?php echo mfo_support_email_link()?></h2></div>
+			</div>
+                        <?php endif?>
+
                 </div><!-- #post-## -->
 
 
